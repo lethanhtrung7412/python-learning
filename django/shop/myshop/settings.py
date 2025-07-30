4352be6e01b3a3a03b2034ea0679dfbedd17ab96
Trung Le <lethanhtrung7412@gmail.com>
2025-07-30 20:01:35 +0700

    adding stripe payment

--- django/shop/myshop/settings.py
+++ django/shop/myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
-    'shop.apps.ShopConfig'
+    'shop.apps.ShopConfig',
+    'payment.apps.PaymentConfig'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_API_VERSION = '2024-04-10'
--- django/shop/orders/models.py
+++ django/shop/orders/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
         return f'Order {self.id}'
     
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.item.all())
+        return sum(item.get_cost() for item in self.items.all())
     
 class OrderItem(models.Model):
     order = models.ForeignKey(
