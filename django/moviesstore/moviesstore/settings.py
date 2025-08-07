5ed180c72e1f7e066fd09cd32e110d5f5fc22e32
Trung Le <lethanhtrung7412@gmail.com>
2025-08-07 18:06:00 +0700

    Adding movies and accounts

--- django/moviesstore/moviesstore/settings.py
+++ django/moviesstore/moviesstore/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'movies',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
